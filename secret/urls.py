67a3aee4e6a491260738603d382bfe6754ac1116
Caio Cavalcante <caio.ayslan.nunes@gmail.com>
2025-02-09 04:09:50 -0300

    finish: CRUD vacancies

--- secret/urls.py
+++ secret/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('platform/', views.platform, name='platform'),
     path('platform/secret_one_vacancie/<int:id>/',views.secret_one_vacancie, name='secret_one_vacancie'),
     path('platform/new_vacancie/',views.new_vacancie, name="new_vacancie"),
-    path('platform/edit_vacancie/<int:id>', views.edit_vacancie, name="edit_vacancie")
+    path('platform/edit_vacancie/<int:id>', views.edit_vacancie, name="edit_vacancie"),
+    path('platform/delete_vacancie/<int:id>', views.delete_vacancie, name="delete_vacancie")
 
 ]
--- secret/views.py
+++ secret/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_enterprise, logout as logout_platform
@@ -9,7 +9,7 @@ from secret.forms import VacancieForm
 from jobs.forms import EnterpriseForm, LoginForm
 from jobs.models import Enterprise, Vacancie
 
-
+# REFERENTE A PARTE EXTERNA DA PLATAFORMA
 def login(request):
 
   if request.method == "GET":
@@ -24,15 +24,12 @@ def login(request):
     new_email = request.POST.get('email')
     new_password = request.POST.get('password')
 
-    # o email informado está sendo validado - vendo se existe mesmo na base de dados
     email = Enterprise.objects.filter(email=new_email).first()
 
-    if email: # se ele existir faz isso aqui ate aqui funciona normal
+    if email:
 
-      # autentica o usuario com os dados informados
       enterprise = authenticate(email=new_email, password=new_password)
 
-      # se a autenticação não der None e a empresa estiver Ativa
       if enterprise is not None and enterprise.is_active:
         login_enterprise(request, enterprise)
         return redirect('platform')
@@ -42,11 +39,10 @@ def login(request):
 
     else:
       return HttpResponse('nao existe na base de dados "email"')
-   
+
 def register(request):
 
   if request.method == "GET":
-
     user = request.user
     
     form = EnterpriseForm()
@@ -86,7 +82,7 @@ def register(request):
       if cnpj_is_valid: # SE O RETORNO FOR TRUE - NÃO CONCLUI O CADASTRO
         return HttpResponse('CNPJ ja existe na base de dados, tente novamente!')
       
-      else: # SE O RETORN FOR FALSE - CRIA O CADASTRO DA EMPRESA - COM SUCESSO
+      else: # SE O RETURN FOR FALSE - CRIA O CADASTRO DA EMPRESA - COM SUCESSO
         new_enterprise = Enterprise.objects.create_user(
           cnpj=cnpj,
           username=username,
@@ -103,11 +99,11 @@ def register(request):
         new_enterprise.save()
         
         return redirect('login')
-    
+
+# VER TODAS AS VAGAS
 @login_required(login_url='login')
 def platform(request): 
 
-    # PEGA A EMPRESA QUE ESTÁ LOGADA
     enterprise_loggedin = request.user
     
     # BUSCA A VAGA EM QUE A EMPRESA QUE CADASTROU == A EMPRESA QUE ESTÁ LOGADA
@@ -119,6 +115,7 @@ def platform(request):
 
     return render(request, template_name='platform.html', context=context)
 
+# VER DETALHES DE UMA VAGA
 @login_required(login_url='login')
 def secret_one_vacancie(request, id):
 
@@ -128,6 +125,7 @@ def secret_one_vacancie(request, id):
 
   return render(request, template_name="secret_one_vacancie.html", context=context)
 
+# CRIA UMA NOVA VAGA
 @login_required(login_url='login')
 def new_vacancie(request):
 
@@ -145,8 +143,6 @@ def new_vacancie(request):
     return render(request, template_name="register_vacancie.html", context=context)
   
   else:
-
-    # COLETA OS DADOS DO FORMULARIO
     title = request.POST.get('title')
     enterprise = Enterprise.objects.get(username=request.user) # PEGA A EMPRESA LOGADA COMO PADRÃO
     email = request.POST.get('email')
@@ -176,26 +172,52 @@ def new_vacancie(request):
 
     return redirect('platform')
 
+# EDITA UMA VAGA
 @login_required(login_url='login')
 def edit_vacancie(request, id):
 
+  vacancie = Vacancie.objects.get(id=id)
+  form = VacancieForm(instance=vacancie)
+
   if request.method == "GET":
-
-    vacancie = Vacancie.objects.get(id=id)
-
-    form = VacancieForm(instance=vacancie)
-
-    context = {
-      'vacancie': vacancie,
-      'form': form
-    }
-
+    context = { 'vacancie': vacancie,'form': form }
     return render(request, template_name='edit_vacancie.html', context=context)
-  
 
-    # Criar Logica Para atualizar a vaga
+  else:
+    title = request.POST.get('title')
+    enterprise = Enterprise.objects.get(username=request.user)  # PEGA A EMPRESA LOGADA COMO PADRÃO
+    email = request.POST.get('email')
+    whatsapp = request.POST.get('whatsapp')
+    wage = request.POST.get('wage')
+    modality = request.POST.get('modality')
+    weekly_journey = request.POST.get('weekly_journey')
+    work_shift = request.POST.get('work_shift')
+    state = request.POST.get('state')
+    description = request.POST.get('description')
 
+    vacancie.title = title
+    vacancie.enterprise = enterprise
+    vacancie.email = email
+    vacancie.whatsapp = whatsapp
+    vacancie.wage = wage
+    vacancie.modality = modality
+    vacancie.weekly_journey = weekly_journey
+    vacancie.work_shift = work_shift
+    vacancie.state = state
+    vacancie.description = description
 
+    vacancie.save()
+
+    return  redirect(f'secret_one_vacancie', id=id)
+
+# DELETA UMA VAGA
+@login_required(login_url='login')
+def delete_vacancie(request, id):
+  vacancie = Vacancie.objects.get(id=id)
+  vacancie.delete()
+  return redirect('platform')
+
+# FAZ O LOGOUT DA CONTA
 @login_required(login_url='login')
 def logout(request):
   logout_platform(request)
